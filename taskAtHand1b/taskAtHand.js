e24e1163fbb73b9420e0982b10e61ab13eb0a65a
Kevin Booth <kdbooth97@gmail.com>
2017-10-22 11:50:48 -0400

    d

--- taskAtHand1b/taskAtHand.js
+++ taskAtHand1b/taskAtHand.js
@@ -2,18 +2,18 @@
 
 // using a function contructor form to create an object
 function TaskAtHandApp()
-{	
-	var version = "v1.0";
+{
+	var version = "v1.2";
 
 	// creating a private function
 	function setStatus(message)
 	{
 		$("#app>footer").text(message);
 	}
-	
+
 	function addTask(){
 		var taskName = $("#new-task-name").val();
-		if (taskName) 
+		if (taskName)
 		{
 			addTaskElement(taskName);
 			//Reset the text field
@@ -21,37 +21,60 @@ function TaskAtHandApp()
 		}
 	}
 	function addTaskElement(taskName) {
-		var $task = $("<li></li>");
-		var $delete = $("<button class='delete'>X</button>");
-		var $moveUp = $("<button class='move-up'>^</button>");
-		var $moveDown = $("<button class='move-down'>V</button>");
-		$task.append($delete)
-			.append($moveUp)
-			.append($moveDown)
-			.append("<span class='task-name'>" + taskName + "</span>");
-		$("#task-list").append($task);	
-		
-		$delete.click(function() { $task.remove(); });
-		$moveUp.click(function() {
+		var $task = $("#task-template .task").clone();
+		$("span.task-name", $task).text(taskName);
+
+		$("#task-list").append($task);
+
+		$("button.delete", $task).click(function() {
+			$task.remove();
+		});
+		$("button.move-up", $task).click(function() {
 			$task.insertBefore($task.prev());
 		});
-		$moveDown.click(function() {
+		$("button.move-down", $task).click(function() {
 			$task.insertAfter($task.next());
 		});
+		$("span.task-name", $task).click(function() {
+			onEditTaskName($(this));
+		});
+		$("input.task-name", $task).change(function() {
+			onChangeTaskName($(this));
+		})
+		.blur(function() {
+			$(this).hide().siblings("span.task-name").show();
+		});
 	}
+	function onEditTaskName($span) {
+		$span.hide()
+			.siblings("input.task-name")
+			.val($span.text())
+			.show()
+			.focus();
+	}
+	function onChangeTaskName($input) {
+		$input.hide();
+		var $span = $input.siblings("span.task-name");
+		if ($input.val())
+		{
+			$span.text($input.val());
+		}
+		$span.show();
+	}
+
 
 	// creating a public function
 	this.start = function()
 	{
 		$("#new-task-name").keypress(function(e) {
-			if (e.which == 13) //Enter key 
+			if (e.which == 13) //Enter key
 			{
 				addTask();
 				return false;
 			}
 		})
 		.focus();
-		
+
 		$("#app>header").append(version);
 		setStatus("ready");
 	};
