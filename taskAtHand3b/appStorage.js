4eeecdf72f79dcc7c74a85faddd56adfc7fa5ee4
Kevin Booth <kdbooth97@gmail.com>
2017-11-13 15:36:54 -0500

    d

--- taskAtHand3b/appStorage.js
+++ taskAtHand3b/appStorage.js
@@ -1,58 +1,82 @@
-function AppStorage(appName) {
-  var prefix = (appName ? appName + "." : "");
-
-  this.localStorageSupported = (('localStorage' in window) && window['localStorage']);
-
-  this.setValue = function(key, val) {
-    if (this.localStorageSupported)
-      localStorage.setItem(prefix + key, JSON.stringify(val));
-    return this;
-  }; // end setValue
-
-  this.getValue = function(key) {
-    if (this.localStorageSupported)
-      return JSON.parse(localStorage.getItem(prefix + key));
-    else return null;
-  };
-
-  this.removeValue = function(key) {
-    if (this.localStorageSupported)
-      localStorage.removeItem(prefix + key);
-    return this;
-  }; // end removeValue
-
-  this.removeAll = function() {
-    var keys = this.getKeys();
-    for (var i in keys) {
-      this.remove(keys[i]);
-    } // end for
-    return this;
-  }; // end removeAll
-
-  this.getKeys = function(filter) {
-    var keys = [];
-    if (this.localStorageSupported) {
-      for (var key in localStorage) {
-        if (isAppKey(key)){
-          if (prefix) key = key.slice(prefix.length);
-
-          if (!filter || filter(key)) {
-            keys.push(key);
-          } // end inner if
-        } // end if
-      } // end for
-    } // end if
-    return keys;
-  }; // end getKeys
-
-  function isAppKey(key) {
-    if (prefix) {
-      return key.indexOf(prefix) === 0;
-    }
-    return true;
-  }; // end isAppKey
-
-  this.contains = function(key) {
-    return this.get(key) !== null;
-  };
-} // end AppStorage
+function AppStorage(appName)
+{
+	var prefix = (appName ? appName + "." : "");
+	
+	this.localStorageSupported = (('localStorage' in window) && window['localStorage']);
+	
+	this.setValue = function(key, val)
+	{
+		if (this.localStorageSupported)
+		{
+			localStorage.setItem(prefix + key, JSON.stringify(val));
+		}
+		return this;
+	};
+	
+	this.getValue = function(key)
+	{
+		if (this.localStorageSupported)
+		{
+			return JSON.parse(localStorage.getItem(prefix + key));
+		}
+		
+		else
+		{
+			return null;
+		}
+	};
+	
+	this.removeValue = function(key)
+	{
+		if (this.localStorageSupported)
+		{
+			localStorage.removeItem(prefix + key);
+		}
+		return this;
+	}
+	
+	this.removeAll = function()
+	{
+		var keys = this.getKeys();
+		for (var i in keys)
+		{
+			this.remove(keys[i]);
+		}
+		return this;
+	};
+	
+	this.getKeys = function(filter)
+	{
+		var keys = [];
+		if (this.localStorageSupported)
+		{
+			for (var keys in localStorage)
+			{
+				if (isAppKey(key))
+				{
+					if (prefix) key = key.slice(prefix.length);
+					
+					if (!filer || filter(key))
+					{
+						keys.push(key);
+					}
+				}
+			}
+		}
+		return keys;
+	};
+	
+	function isAppKey(key)
+	{
+		if (prefix)
+		{
+			return key.indexOf(prefix) === 0;
+		}
+		return true;
+	};
+	
+	this.contains = function(key)
+	{
+		return this.get(key) !== null;
+	};
+};		
