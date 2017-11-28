e806193228e86d973bc63be3302b5a5c1483e9b0
Kevin Booth <kdbooth97@gmail.com>
2017-11-27 23:06:00 -0500

    j
    j

---
+++ apiGame1b/game.js
@@ -0,0 +1,72 @@
+"use strict";
+
+function JeopardyApp() {
+
+  function nextGame() {
+    var gameCount = gameCount + 1;
+    
+  }
+  function getData() {
+    $("#answer").val("");
+    var answer;
+     $.ajax({
+      type: "GET",
+      url: "http://jservice.io/api/random",
+      dataType: "json"
+    })
+    .done(function(data) { appendData(data); })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+					showError(errorThrown);
+    });
+  }
+
+  function appendData(data) {
+      $("#title").text(data[0].category.title);
+      $("#diff").text(data[0].value);
+      $("#question").text(data[0].question);
+      $("#answer").text(data[0].answer).hide();
+  }
+
+  function appendFooter(message) {
+    $("footer").text(message)
+  }
+
+  function validateAnswer(number) {
+    if (number = 1) {
+      var count = parseInt($("#score").text());
+      count = count + 1;
+      $("#score").text(count);
+    }
+  }
+
+  this.start = function() {
+    $("#hidden").hide();
+
+    appendFooter("Jeopardy Game Version 1.2");
+    //making ajax call
+    $("#getquest").click(function() {
+    getData();
+    $("#input").val("");
+    });
+    //on click show answer etc..
+    $("#submit").click(function() {
+      $("#answer").show();
+      $("#hidden").show();
+    });
+    //incrementing the count of score
+    $("#yes").click(function() {
+      validateAnswer(1);
+      $("#hidden").hide();
+    });
+    //hiding bottom area for next question
+    $("#no").click(function() {
+      $("#hidden").hide();
+    });
+  };
+
+} // end jeopardyApp
+
+$(function() {
+	window.app = new JeopardyApp();
+	window.app.start();
+});
